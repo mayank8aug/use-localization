dcb19e95bb303295d84b748582f1ad4a6232da5d
mayank8aug <mayank8aug@gmail.com>
2021-04-15 20:29:56 +0530

    Added TS

--- src/utils.ts
+++ src/utils.ts
@@ -110,110 +110,110 @@ export const currencies: { [key: string]: string } = {
 export const rtlLangs = ['ar', 'fa', 'he', 'ps', 'ur'];
 
 export enum LOCALES {
-    'sq-AL' = 'ALL', // ALBANIA
-    'ar-DZ' = 'DZD', // ALGERIA
-    'ar-BH' = 'BHD', // BAHRAIN
-    'ar-EG' = 'EGP', // EGYPT
-    'ar-IQ' = 'IQD', // IRAQ
-    'ar-JO' = 'JOD', // JORDAN
-    'ar-KW' = 'KWD', // KUWAIT
-    'ar-LB' = 'LBP', // LEBANON
-    'ar-LY' = 'LYD', // LIBYA
-    'ar-MA' = 'MAD', // MOROCCO
-    'ar-OM' = 'OMR', // OMAN
-    'ar-QA' = 'QAR', // QATAR
-    'ar-SA' = 'SAR', // SAUDI ARABIA
-    'ar-SD' = 'SDG', // SUDAN
-    'ar-SY' = 'SYP', // SYRIA
-    'ar-TN' = 'TND', // TUNISIA
-    'ar-AE' = 'AED', // UAE
-    'ar-YE' = 'YER', // YEMEN
-    'be-BY' = 'BYN', // BELARUS
-    'bg-BG' = 'BGN', // BULGARIA
-    'ca-ES' = 'EUR', // SPAIN
-    'zh-CN' = 'CNY', // CHINA
-    'zh-HK' = 'HKD', // HONG KONG
-    'zh-SG' = 'SGD', // SINGAPORE
-    'zh-TW' = 'TWD', // TAIWAN
-    'hr-HR' = 'HRK', // CROATIA
-    'cs-CZ' = 'CZK', // CZECH REPUBLIC
-    'da-DK' = 'DKK', // DENMARK
-    'nl-BE' = 'EUR', // BELIGIUM
-    'nl-NL' = 'EUR', // NETHERLANDS
-    'en-AU' = 'AUD', // AUSTRALIA
-    'en-CA' = 'CAD', // CANADA
-    'en-IN' = 'INR', // INDIA
-    'en-IE' = 'EUR', // IRELAND
-    'en-MT' = 'EUR', // MALTA
-    'en-NZ' = 'NZD', // NEW ZEALAND
-    'en-PH' = 'PHP', // PHILIPPINES
-    'en-SG' = 'SGD', // SINGAPORE
-    'en-ZA' = 'ZAR', // SOUTH AFRICA
-    'en-GB' = 'GBP', // UNITED KINGDOM
-    'en-US' = 'USD', // US
-    'et-EE' = 'EUR', // ESTONIA
-    'fi-FI' = 'EUR', // FINLAND
-    'fr-BE' = 'EUR', // BELGIUM
-    'fr-CA' = 'CAD', // CANADA
-    'fr-FR' = 'EUR', // FRANCE
-    'fr-LU' = 'EUR', // LUXEMBOURG
-    'fr-CH' = 'CHF', // SWITZERLAND
-    'de-AT' = 'EUR', // AUSTRIA
-    'de-DE' = 'EUR', // GERMANY
-    'de-LU' = 'EUR', // LUXEMBOURG
-    'de-CH' = 'CHF', // SWITZERLAND
-    'el-CY' = 'EUR', // CYPRUS
-    'el-GR' = 'EUR', // GREECE
-    'iw-IL' = 'ILS', // ISRAEL
-    'hi-IN' = 'INR', // INDIA
-    'hu-HU' = 'HUF', // HUNGARY
-    'is-IS' = 'ISK', // ICELAND
-    'in-ID' = 'IDR', // INDONESIA
-    'ga-IE' = 'EUR', // IRELAND
-    'it-IT' = 'EUR', // ITALY
-    'it-CH' = 'CHF', // SWITZERLAND
-    'ja-JP' = 'JPY',  // JAPAN
-    'ko-KR' = 'KRW', // SOUTH KOREA
-    'lv-LV' = 'EUR', // LATVIA
-    'lt-LT' = 'EUR', // LITHUANIA
-    'mk-MK' = 'MKD', // MACEDONIA
-    'ms-MY' = 'MYR', // MALAYSIA
-    'mt-MT' = 'EUR', // MALTA
-    'no-NO' = 'NOK', // NORWAY
-    'pl-PL' = 'PLN', // POLAND
-    'pt-BR' = 'BRL', // BRAZIL
-    'pt-PT' = 'EUR', // PORTUGAL
-    'ro-RO' = 'RON', // ROMANIA
-    'ru-RU' = 'RUB', // RUSSIA
-    'sr-BA' = 'BAM', // BOSNIA AND HERZEGOVINA
-    'sr-ME' = 'EUR', // MONTENEGRO
-    'sr-CS' = 'EURO', // SERBIA AND MONTENEGRO
-    'sr-RS' = 'RSD', // SERBIA
-    'sk-SK' = 'EUR', // SLOVAKIA
-    'sl-SI' = 'EUR', // SLOVENIA
-    'es-AR' = 'ARS', // ARGENTINA
-    'es-BO' = 'BOB', // BOLIVIA
-    'es-CL' = 'CLP', // CHILE
-    'es-CO' = 'COP', // COLOMBIA
-    'es-CR' = 'CRC', // COSTA RICA
-    'es-DO' = 'DOP', // DOMINICAN REPUBLIC
-    'es-EC' = 'USD', // ECUADOR
-    'es-SV' = 'USD', // EL SALVADOR
-    'es-GT' = 'GTQ', // GUATEMALA
-    'es-HN' = 'HNL', // HONDURAS
-    'es-MX' = 'MXN', // MEXICO
-    'es-NI' = 'NIO', // NICARAGUA
-    'es-PA' = 'USD', // PANAMA
-    'es-PY' = 'PYG', // PARAGUAY
-    'es-PE' = 'PEN', // PERU
-    'es-PR' = 'USD', // PUERTO RICO
-    'es-ES' = 'EUR', // SPAIN
-    'es-US' = 'USD', // US
-    'es-UY' = 'UYU', // URUGUAY
-    'es-VE' = 'VEF', // VENEZUELA
-    'sv-SE' = 'SEK', // SWEDEN
-    'th-TH' = 'THB', // THAILAND
-    'tr-TR' = 'TRY', // TURKEY
-    'uk-UA' = 'UAH', // UKRAINE
-    'vi-VN' = 'VND', // VIETNAM
+    'sq-AL',
+    'ar-DZ',
+    'ar-BH',
+    'ar-EG',
+    'ar-IQ',
+    'ar-JO',
+    'ar-KW',
+    'ar-LB',
+    'ar-LY',
+    'ar-MA',
+    'ar-OM',
+    'ar-QA',
+    'ar-SA',
+    'ar-SD',
+    'ar-SY',
+    'ar-TN',
+    'ar-AE',
+    'ar-YE',
+    'be-BY',
+    'bg-BG',
+    'ca-ES',
+    'zh-CN',
+    'zh-HK',
+    'zh-SG',
+    'zh-TW',
+    'hr-HR',
+    'cs-CZ',
+    'da-DK',
+    'nl-BE',
+    'nl-NL',
+    'en-AU',
+    'en-CA',
+    'en-IN',
+    'en-IE',
+    'en-MT',
+    'en-NZ',
+    'en-PH',
+    'en-SG',
+    'en-ZA',
+    'en-GB',
+    'en-US',
+    'et-EE',
+    'fi-FI',
+    'fr-BE',
+    'fr-CA',
+    'fr-FR',
+    'fr-LU',
+    'fr-CH',
+    'de-AT',
+    'de-DE',
+    'de-LU',
+    'de-CH',
+    'el-CY',
+    'el-GR',
+    'iw-IL',
+    'hi-IN',
+    'hu-HU',
+    'is-IS',
+    'in-ID',
+    'ga-IE',
+    'it-IT',
+    'it-CH',
+    'ja-JP',
+    'ko-KR',
+    'lv-LV',
+    'lt-LT',
+    'mk-MK',
+    'ms-MY',
+    'mt-MT',
+    'no-NO',
+    'pl-PL',
+    'pt-BR',
+    'pt-PT',
+    'ro-RO',
+    'ru-RU',
+    'sr-BA',
+    'sr-ME',
+    'sr-CS',
+    'sr-RS',
+    'sk-SK',
+    'sl-SI',
+    'es-AR',
+    'es-BO',
+    'es-CL',
+    'es-CO',
+    'es-CR',
+    'es-DO',
+    'es-EC',
+    'es-SV',
+    'es-GT',
+    'es-HN',
+    'es-MX',
+    'es-NI',
+    'es-PA',
+    'es-PY',
+    'es-PE',
+    'es-PR',
+    'es-ES',
+    'es-US',
+    'es-UY',
+    'es-VE',
+    'sv-SE',
+    'th-TH',
+    'tr-TR',
+    'uk-UA',
+    'vi-VN',
 }
